b606de7ce58c2e768fbcd18097fe63e27b08cbfe
Ceren Karakoç <02cerenkarakoc@gmail.com>
2024-04-04 14:07:03 +0300

    .

---
+++ src/container/Intro/Intro.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { meal } from '../../constants'
+import './Intro.css';
+
+const Intro = () => (
+  <div>
+    Intro
+  </div>
+);
+
+export default Intro;
